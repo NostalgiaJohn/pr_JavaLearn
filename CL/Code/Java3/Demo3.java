200b83acd899cf146e6302bbf3e61d3c5b4ed5b4
fStardust <luqi234@163.com>
2021-04-23 14:44:58 +0800

    while & do while

---
+++ CL/Code/Java3/Demo3.java
@@ -0,0 +1,17 @@
+class Demo3 {
+	public static void main(String[] args) {
+		// while—≠ª∑Ω·ππ
+		int i = 1;
+		
+		while (i <= 100) {
+			System.out.println(i);
+			i += 1;
+		}
+		
+		int j = 1;
+		do {
+			System.out.println(j);
+			j += 1;
+		} while (j <= 100);
+	}
+}
