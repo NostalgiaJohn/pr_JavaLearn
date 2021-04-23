cb83a5960e364347ab1797b45d506baeac45cd7c
fStardust <luqi234@163.com>
2021-04-23 22:06:54 +0800

    method1

---
+++ CL/Code/Java4/Method1.java
@@ -0,0 +1,27 @@
+
+class Method1 {
+	
+	public static void main(String[] args) {
+		// 方法名() --调用方法
+		bbq();
+	}
+	/*
+	需求
+		打印一个“烤羊排”
+		
+	方法分析
+		public static 固定格式
+		返回值类型：void
+		方法名：bbq
+		形式参数列表：()
+		
+	方法声明：
+		
+	*/
+	/**
+	* 打印烤羊排
+	*/
+	public static void bbq() {
+		System.out.println("烤羊排");
+	}
+}
